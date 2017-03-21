8f44d2d8356c4df0281bad4864eea2cb7315a999
Piotr Muzyka <piotrmuzyka@yahoo.com>
2017-03-21 23:52:36 +0100

    Added increment and decrement methods. Order button allows to calculate the price.

--- app/src/main/java/com/example/android/justjava/JustJava.java
+++ app/src/main/java/com/example/android/justjava/JustJava.java
@@ -13,32 +13,42 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class JustJava extends AppCompatActivity {
-
+    int orderQuantity=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_just_java);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
+    // This method is called when the order button is clicked.
+
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(1*10);
+        displayPrice(orderQuantity*10);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+    //This method increments the order quantity.
+
+    public void increment(View view) {
+        orderQuantity+=1;
+        display(orderQuantity);
+    }
+
+    //This method decrements the order quantity.
+
+    public void decrement(View view) {
+        orderQuantity-=1;
+        display(orderQuantity);
+    }
+
+    //This method displays the given quantity value on the screen.
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
+    //This method displays the given price on the screen.
+
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
